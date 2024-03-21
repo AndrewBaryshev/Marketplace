0c1559e95e17f2d3f39ca39e56cb9f785f84413c
a.baryshev <a.baryshev@astondevs.ru>
2024-03-21 03:07:10 +0300

    cards-component fetch

--- src/components/cards-component/CardsComponent.tsx
+++ src/components/cards-component/CardsComponent.tsx
@@ -5,14 +5,25 @@ import styles from "./cards-component.module.css";
 export const CardsComponent = () => {
   const [data, setData] = useState([]);
 
+  async function getData(url: any) {
+    try {
+      const promise = await fetch(url);
+      const res = await promise.json();
+      console.log(res);
+      setData(res);
+    } catch (err) {
+      console.log("fuck: ", err);
+    }
+  }
+
   useEffect(() => {
-    fetch(
+    getData(
       "https://bh018dd7r3.execute-api.us-west-2.amazonaws.com/api/shop/products"
-    )
-      .then((data) => data.json())
-      .then((res) => setData(res));
+    );
     console.log("here");
   }, []);
+
+  console.log(data);
 
   return (
     <div className={styles.cards}>
